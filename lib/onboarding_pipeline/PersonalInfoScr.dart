60f6dc09a17fb57442c99b6d06cf52074e5c112e
Burac, Samuel R <sburac3@gatech.edu>
2024-05-20 15:32:59 -0400

    personal info screen done

--- lib/onboarding_pipeline/PersonalInfoScr.dart
+++ lib/onboarding_pipeline/PersonalInfoScr.dart
@@ -9,31 +9,100 @@ class PersonalInfoScr extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Funny Joke App'),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "_currentJoke",
-                style: TextStyle(fontSize: 24.0, fontStyle: FontStyle.italic),
-                textAlign: TextAlign.center,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const Text(
+              "Welcome to helping hand!",
+              style: TextStyle(fontSize: 40.0),
+            ),
+            const Text(
+              "Lets get a little more info to get you started.",
+              style: TextStyle(fontSize: 20),
+            ),
+            const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  "Where are you located?",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "ZIP code / City, State",
+                    ),
+                  ),
+                ),
+                Text(
+                  "This is used to sort jobs based on distance",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                CheckboxListTile(
+              title: const Text(
+                "Looking for work?",
+                style: TextStyle(fontSize: 20),
               ),
-              const SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () {
-                  // _getJoke();
-                },
-                child: const Text('Get a Joke'),
+              controlAffinity: ListTileControlAffinity.trailing,
+              value: false,
+              onChanged: (bool? value) {
+                peeHolder(true);
+              },
+            ),
+            CheckboxListTile(
+              title: const Text(
+                "Looking for workers?",
+                style: TextStyle(fontSize: 20),
               ),
-            ],
-          ),
+              controlAffinity: ListTileControlAffinity.trailing,
+              value: false,
+              onChanged: (bool? value) {
+                peeHolder(true);
+              },
+            ),
+              ],
+            ),
+            
+            Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Divider(
+                  color: Colors.grey,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/profileSetup');
+                      },
+                      icon: const Icon(Icons.arrow_forward),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+void peeHolder(bool pee) {
+  print("bruh");
+}
+
+mixin val {}
--- lib/onboarding_pipeline/SignupScr.dart
+++ lib/onboarding_pipeline/SignupScr.dart
@@ -31,11 +31,11 @@ class EmailSignupScr extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  buildTextField("First Name"),
-                  buildTextField("Last Name"),
-                  buildTextField("Email"),
-                  buildTextField("Phone Number"),
-                  buildTextField("Password")
+                  buildTextField("First Name", false),
+                  buildTextField("Last Name", false),
+                  buildTextField("Email", false),
+                  buildTextField("Phone Number", false),
+                  const CustomTextField(labelText: 'Password', obscureText: true),
                 ],
               ),
             ),
@@ -92,7 +92,8 @@ class EmailSignupScr extends StatelessWidget {
                                     ),
                                   ),
                                   minimumSize: WidgetStateProperty.all<Size>(
-                                      const Size(0, 50)), // set the height as needed
+                                      const Size(
+                                          0, 50)), // set the height as needed
                                 ),
                         child: const Text("Create Account"),
                       ),
@@ -108,8 +109,9 @@ class EmailSignupScr extends StatelessWidget {
   }
 }
 
-TextField buildTextField(String labelText) {
+TextField buildTextField(String labelText, bool obscureText) {
   return TextField(
+    obscureText: obscureText,
     cursorColor: Colors.orange,
     decoration: InputDecoration(
       border: OutlineInputBorder(
@@ -127,4 +129,68 @@ TextField buildTextField(String labelText) {
       labelText: labelText,
     ),
   );
+}
+
+class CustomTextField extends StatefulWidget {
+  final String labelText;
+  final bool obscureText;
+
+  const CustomTextField({super.key, required this.labelText, this.obscureText = false});
+
+  @override
+  _CustomTextFieldState createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  late bool _obscureText;
+  final TextEditingController _controller = TextEditingController();
+  bool _hasInput = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _obscureText = widget.obscureText;
+    _controller.addListener(() {
+      setState(() {
+        _hasInput = _controller.text.isNotEmpty;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _controller,
+      obscureText: _obscureText,
+      cursorColor: Colors.orange,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        floatingLabelStyle: const TextStyle(
+          color: Colors.orange,
+        ),
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Colors.orange,
+            width: 1.0,
+          ),
+        ),
+        labelText: widget.labelText,
+        suffixIcon: widget.obscureText && _hasInput
+            ? IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility : Icons.visibility_off,
+                  color: Colors.orange,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _obscureText = !_obscureText;
+                  });
+                },
+              )
+            : null,
+      ),
+    );
+  }
 }
